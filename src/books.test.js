1d79823789824bf6b3a543f193ec84bd7f2b9a92
philscrace2 <42113748+philscrace2@users.noreply.github.com>
2023-03-03 15:38:00 +0000

    Tests works

--- src/books.test.js
+++ src/books.test.js
@@ -1,4 +1,29 @@
 import BooksPresenter from "./Books/BooksPresenter";
 import booksRepository from "./Books/BooksRepository";
+import httpGateway from "./Shared/HttpGateway";
 
-it("hello", () => {});
+
+it("should hit backend API and load 3 viewmodel books when 3 books loaded from the backend", async() => {
+
+    httpGateway.get = jest.fn().mockImplementation(() => {
+        return {"success":true,"result":[{"bookId":42191,"name":"Wind in the willows","ownerId":"philscrace@gmail.com","author":"Kenneth Graeme"},{"bookId":42201,"name":"I, Robot","ownerId":"philscrace@gmail.com","author":"Isaac Asimov"},{"bookId":42211,"name":"The Hobbit","ownerId":"philscrace@gmail.com","author":"Jrr Tolkein"}]}
+    });
+
+    let viewModel = null;
+    let booksPresenter = new BooksPresenter();
+
+    await booksPresenter.load(generatedViewModel => {
+        viewModel = generatedViewModel;
+    });
+
+    expect(httpGateway.get).toHaveBeenCalledWith("https://api.logicroom.co/api/philscrace@gmail.com/books");
+
+    expect(viewModel.length).toBe(3);
+
+
+});
+
+it("Should be true!", async() => {
+    expect(true).toBe(true);
+
+});
